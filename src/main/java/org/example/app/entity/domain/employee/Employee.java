428b078d5b2b6fe07fd7121a95f0d3a23f1fdf59
KaterynaDubinina <ekaterynadubinina@gmail.com>
2024-03-30 15:17:02 +0200

    Initial commit

---
+++ src/main/java/org/example/app/entity/domain/employee/Employee.java
@@ -0,0 +1,25 @@
+package org.example.app.entity.domain.employee;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "surname")
+    private String surname;
+    @Column(name = "position")
+    private String position;
+    @Column(name = "phone")
+    private String phone;
+}
